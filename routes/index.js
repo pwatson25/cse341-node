bf95aec3f1b1e581e257826874a3e509710bd34c
peterjwatson <peterskyguy@gmail.com>
2022-10-15 19:20:55 -0400

    idk man...

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/contacts', require('./contacts'))
 router.use('/', require('./docs'));
+router.use('/contacts', require('./contacts'))
 
 // routes.get('/', (req, res) => {
 //     res.send('Joshua Watson')
